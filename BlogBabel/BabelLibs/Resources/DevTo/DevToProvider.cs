d2af5bb09ba41da159ddba48a014de187b87a399
Tsuyoshi Ushio <ushio@simplearchitect.com>
2024-01-12 22:09:16 -0800

    Initial implementation of DevTo provider.

--- BlogBabel/BabelLibs/Resources/DevTo/DevToProvider.cs
+++ BlogBabel/BabelLibs/Resources/DevTo/DevToProvider.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BabelLibs.Resources.DevTo
 {
     public class DevToProvider
     {
+        private readonly HttpClient _httpClient;
+        private readonly DevToSettings _settings;
+        public DevToProvider(IOptions<DevToSettings> settings)
+        {
+            _settings = settings.Value;
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("api-key", $"{_settings.ApiKey}");
+        }
+
+        public async Task PostAsync(GenericPost post)
+        {
+            var content = new StringContent(JsonSerializer.Serialize(new
+            {
+                article = new
+                {
+                    post.Title,
+                    body_markdown = post.Body,
+                    published = false,
+                    description = post.Title,
+                    tags = new[] { _settings.Sentiment }
+                }
+            }), Encoding.UTF8, "application/json");
+
+            using var response = await _httpClient.PostAsync("https://dev.to/api/articles", content);
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
---
+++ BlogBabel/BabelLibs/Resources/GenericPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabelLibs.Resources
+{
+    public class GenericPost
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public IList<string> _Tags { get; set; } = new List<string>();
+    }
+}
