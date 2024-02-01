c3eabc38b68895292ffeee1f35ee3148595ab854
Tsuyoshi Ushio <ushio@simplearchitect.com>
2024-01-31 21:03:52 -0800

    Update the unit test adapt the real GitHub path.

--- BlogBabel/BabelLibs.Tests/MarkdownUtilsTests.cs
+++ BlogBabel/BabelLibs.Tests/MarkdownUtilsTests.cs
@@ -11,18 +11,29 @@ namespace BabelLibs.Tests
         {
             string scenario = nameof(MarkdownUtilsTests);
             string testcase = nameof(ConvertPathNormalCase);
+            string unitTestProjectRoot = GetUnitTestProjectRoot();
 
             string SourceContent = ReadAllText(scenario, testcase, "Source.md");
-            string sourcePath = Path.Combine("fixture", scenario, testcase, "Source.md");
+            string sourcePath = Path.Combine(unitTestProjectRoot, "fixture", scenario, testcase, "Source.md");
             string ExpectedContent = ReadAllText(scenario, testcase, "Expected.md");
+
+            // sourcePath can be relative/absolute path. The test runner runs in the debug directory, so we need to use the absolute path.
             string acttualContent = MarkdownUtils.ConvertPath(SourceContent, sourcePath, $"{BASE_URL}");
             Assert.Equal(ExpectedContent, acttualContent);
         }
 
         private static string ReadAllText(string scenario, string testcase, string targetPath)
         {
-            string path = Path.Combine("fixture", scenario, testcase, targetPath);
+
+            string unitTestProjectRoot = GetUnitTestProjectRoot();
+            string path = Path.Combine(unitTestProjectRoot, "fixture", scenario, testcase, targetPath);
             return File.ReadAllText(path);
+        }
+
+        private static string GetUnitTestProjectRoot()
+        {
+            string projectRoot = MarkdownUtils.GetGitRoot(Directory.GetCurrentDirectory());
+            return Path.Combine(projectRoot, "BlogBabel", "BabelLibs.Tests");
         }
     }
 }
--- BlogBabel/BabelLibs.Tests/fixture/MarkdownUtilsTests/ConvertPathNormalCase/Expected.md
+++ BlogBabel/BabelLibs.Tests/fixture/MarkdownUtilsTests/ConvertPathNormalCase/Expected.md
@@ -4,4 +4,4 @@ This file contains the test patterns for the `MarkdownUtils` class.
 
 ## Normal replacement
 
-* ![Normal Image](https://raw.githubusercontent.com/TsuyoshiUshio/BlogBabel/main/BlogBabel/BabelLibs.Tests/bin/Debug/net8.0/fixture/MarkdownUtilsTests/ConvertPathNormalCase/img/blogbabel-icon.jpg)
+* ![Normal Image](https://raw.githubusercontent.com/TsuyoshiUshio/BlogBabel/main/BlogBabel/BabelLibs.Tests/fixture/MarkdownUtilsTests/ConvertPathNormalCase/img/blogbabel-icon.jpg)
